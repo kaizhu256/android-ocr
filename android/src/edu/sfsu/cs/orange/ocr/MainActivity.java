e426a013286f3545428ef3d8533d26579dc44b39
kai zhu <kaizhu256@gmail.com>
2014-07-13 05:00:03 +0800

    delete ocr boxes

--- android/src/edu/sfsu/cs/orange/ocr/MainActivity.java
+++ android/src/edu/sfsu/cs/orange/ocr/MainActivity.java
@@ -1242,29 +1242,8 @@ public final class MainActivity extends Activity implements SurfaceHolder.Callba
 class ViewfinderView extends View {
   //private static final long ANIMATION_DELAY = 80L;
 
-  /** Flag to draw boxes representing the results from TessBaseAPI::GetRegions(). */
-  static final boolean DRAW_REGION_BOXES = false;
-
-  /** Flag to draw boxes representing the results from TessBaseAPI::GetTextlines(). */
-  static final boolean DRAW_TEXTLINE_BOXES = true;
-
-  /** Flag to draw boxes representing the results from TessBaseAPI::GetStrips(). */
-  static final boolean DRAW_STRIP_BOXES = false;
-
   /** Flag to draw boxes representing the results from TessBaseAPI::GetWords(). */
   static final boolean DRAW_WORD_BOXES = true;
-
-  /** Flag to draw word text with a background varying from transparent to opaque. */
-  static final boolean DRAW_TRANSPARENT_WORD_BACKGROUNDS = false;
-
-  /** Flag to draw boxes representing the results from TessBaseAPI::GetCharacters(). */
-  static final boolean DRAW_CHARACTER_BOXES = false;
-
-  /** Flag to draw the text of words within their respective boxes from TessBaseAPI::GetWords(). */
-  static final boolean DRAW_WORD_TEXT = false;
-
-  /** Flag to draw each character in its respective box from TessBaseAPI::GetCharacters(). */
-  static final boolean DRAW_CHARACTER_TEXT = false;
 
   private CameraManager cameraManager;
   private final Paint paint;
@@ -1331,63 +1310,9 @@ class ViewfinderView extends View {
         float scaleX = frame.width() / (float) previewFrame.width();
         float scaleY = frame.height() / (float) previewFrame.height();
 
-        if (DRAW_REGION_BOXES) {
-          regionBoundingBoxes = resultText.getRegionBoundingBoxes();
-          for (int i = 0; i < regionBoundingBoxes.size(); i++) {
-            paint.setAlpha(0xA0);
-            paint.setColor(Color.MAGENTA);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(1);
-            rect = regionBoundingBoxes.get(i);
-            canvas.drawRect(frame.left + rect.left * scaleX,
-              frame.top + rect.top * scaleY,
-              frame.left + rect.right * scaleX,
-              frame.top + rect.bottom * scaleY, paint);
-          }
-        }
-
-        if (DRAW_TEXTLINE_BOXES) {
-          // Draw each textline
-          textlineBoundingBoxes = resultText.getTextlineBoundingBoxes();
-          paint.setAlpha(0xA0);
-          paint.setColor(Color.RED);
-          paint.setStyle(Paint.Style.STROKE);
-          paint.setStrokeWidth(1);
-          for (int i = 0; i < textlineBoundingBoxes.size(); i++) {
-            rect = textlineBoundingBoxes.get(i);
-            canvas.drawRect(frame.left + rect.left * scaleX,
-              frame.top + rect.top * scaleY,
-              frame.left + rect.right * scaleX,
-              frame.top + rect.bottom * scaleY, paint);
-          }
-        }
-
-        if (DRAW_STRIP_BOXES) {
-          stripBoundingBoxes = resultText.getStripBoundingBoxes();
-          paint.setAlpha(0xFF);
-          paint.setColor(Color.YELLOW);
-          paint.setStyle(Paint.Style.STROKE);
-          paint.setStrokeWidth(1);
-          for (int i = 0; i < stripBoundingBoxes.size(); i++) {
-            rect = stripBoundingBoxes.get(i);
-            canvas.drawRect(frame.left + rect.left * scaleX,
-              frame.top + rect.top * scaleY,
-              frame.left + rect.right * scaleX,
-              frame.top + rect.bottom * scaleY, paint);
-          }
-        }
-
-        if (DRAW_WORD_BOXES || DRAW_WORD_TEXT) {
+        if (DRAW_WORD_BOXES) {
           // Split the text into words
           wordBoundingBoxes = resultText.getWordBoundingBoxes();
-          //      for (String w : words) {
-          //        Log.e("ViewfinderView", "word: " + w);
-          //      }
-          //Log.d("ViewfinderView", "There are " + words.length + " words in the string array.");
-          //Log.d("ViewfinderView", "There are " + wordBoundingBoxes.size() + " words with bounding boxes.");
-        }
-
-        if (DRAW_WORD_BOXES) {
           paint.setAlpha(0xFF);
           paint.setColor(0xFF00CCFF);
           paint.setStyle(Paint.Style.STROKE);
@@ -1403,148 +1328,6 @@ class ViewfinderView extends View {
           }
         }
 
-        if (DRAW_WORD_TEXT) {
-          words = resultText.getText().replace("\n", " ").split(" ");
-          int[] wordConfidences = resultText.getWordConfidences();
-          for (int i = 0; i < wordBoundingBoxes.size(); i++) {
-            boolean isWordBlank = true;
-            try {
-              if (!words[i].equals("")) {
-                isWordBlank = false;
-              }
-            } catch (ArrayIndexOutOfBoundsException e) {
-              e.printStackTrace();
-            }
-
-            // Only draw if word has characters
-            if (!isWordBlank) {
-              // Draw a white background around each word
-              rect = wordBoundingBoxes.get(i);
-              paint.setColor(Color.WHITE);
-              paint.setStyle(Paint.Style.FILL);
-              if (DRAW_TRANSPARENT_WORD_BACKGROUNDS) {
-                // Higher confidence = more opaque, less transparent background
-                paint.setAlpha(wordConfidences[i] * 255 / 100);
-              } else {
-                paint.setAlpha(255);
-              }
-              canvas.drawRect(frame.left + rect.left * scaleX,
-                frame.top + rect.top * scaleY,
-                frame.left + rect.right * scaleX,
-                frame.top + rect.bottom * scaleY, paint);
-
-              // Draw the word in black text
-              paint.setColor(Color.BLACK);
-              paint.setAlpha(0xFF);
-              paint.setAntiAlias(true);
-              paint.setTextAlign(Paint.Align.LEFT);
-
-              // Adjust text size to fill rect
-              paint.setTextSize(100);
-              paint.setTextScaleX(1.0f);
-              // ask the paint for the bounding rect if it were to draw this text
-              Rect bounds = new Rect();
-              paint.getTextBounds(words[i], 0, words[i].length(), bounds);
-              // get the height that would have been produced
-              int h = bounds.bottom - bounds.top;
-              // figure out what textSize setting would create that height of text
-              float size = (((float)(rect.height()) / h) * 100f);
-              // and set it into the paint
-              paint.setTextSize(size);
-              // Now set the scale.
-              // do calculation with scale of 1.0 (no scale)
-              paint.setTextScaleX(1.0f);
-              // ask the paint for the bounding rect if it were to draw this text.
-              paint.getTextBounds(words[i], 0, words[i].length(), bounds);
-              // determine the width
-              int w = bounds.right - bounds.left;
-              // calculate the baseline to use so that the entire text is visible including the descenders
-              int text_h = bounds.bottom - bounds.top;
-              int baseline = bounds.bottom + ((rect.height() - text_h) / 2);
-              // determine how much to scale the width to fit the view
-              float xscale = ((float)(rect.width())) / w;
-              // set the scale for the text paint
-              paint.setTextScaleX(xscale);
-              canvas.drawText(words[i], frame.left + rect.left * scaleX, frame.top + rect.bottom * scaleY - baseline, paint);
-            }
-
-          }
-        }
-
-        //        if (DRAW_CHARACTER_BOXES || DRAW_CHARACTER_TEXT) {
-        //          characterBoundingBoxes = resultText.getCharacterBoundingBoxes();
-        //        }
-        //
-        //        if (DRAW_CHARACTER_BOXES) {
-        //          // Draw bounding boxes around each character
-        //          paint.setAlpha(0xA0);
-        //          paint.setColor(0xFF00FF00);
-        //          paint.setStyle(Style.STROKE);
-        //          paint.setStrokeWidth(1);
-        //          for (int c = 0; c < characterBoundingBoxes.size(); c++) {
-        //            Rect characterRect = characterBoundingBoxes.get(c);
-        //            canvas.drawRect(frame.left + characterRect.left * scaleX,
-        //                frame.top + characterRect.top * scaleY,
-        //                frame.left + characterRect.right * scaleX,
-        //                frame.top + characterRect.bottom * scaleY, paint);
-        //          }
-        //        }
-        //
-        //        if (DRAW_CHARACTER_TEXT) {
-        //          // Draw letters individually
-        //          for (int i = 0; i < characterBoundingBoxes.size(); i++) {
-        //            Rect r = characterBoundingBoxes.get(i);
-        //
-        //            // Draw a white background for every letter
-        //            int meanConfidence = resultText.getMeanConfidence();
-        //            paint.setColor(Color.WHITE);
-        //            paint.setAlpha(meanConfidence * (255 / 100));
-        //            paint.setStyle(Style.FILL);
-        //            canvas.drawRect(frame.left + r.left * scaleX,
-        //                frame.top + r.top * scaleY,
-        //                frame.left + r.right * scaleX,
-        //                frame.top + r.bottom * scaleY, paint);
-        //
-        //            // Draw each letter, in black
-        //            paint.setColor(Color.BLACK);
-        //            paint.setAlpha(0xFF);
-        //            paint.setAntiAlias(true);
-        //            paint.setTextAlign(Align.LEFT);
-        //            String letter = "";
-        //            try {
-        //              char c = resultText.getText().replace("\n","").replace(" ", "").charAt(i);
-        //              letter = Character.toString(c);
-        //
-        //              if (!letter.equals("-") && !letter.equals("_")) {
-        //
-        //                // Adjust text size to fill rect
-        //                paint.setTextSize(100);
-        //                paint.setTextScaleX(1.0f);
-        //
-        //                // ask the paint for the bounding rect if it were to draw this text
-        //                Rect bounds = new Rect();
-        //                paint.getTextBounds(letter, 0, letter.length(), bounds);
-        //
-        //                // get the height that would have been produced
-        //                int h = bounds.bottom - bounds.top;
-        //
-        //                // figure out what textSize setting would create that height of text
-        //                float size  = (((float)(r.height())/h)*100f);
-        //
-        //                // and set it into the paint
-        //                paint.setTextSize(size);
-        //
-        //                // Draw the text as is. We don't really need to set the text scale, because the dimensions
-        //                // of the Rect should already be suited for drawing our letter.
-        //                canvas.drawText(letter, frame.left + r.left * scaleX, frame.top + r.bottom * scaleY, paint);
-        //              }
-        //            } catch (StringIndexOutOfBoundsException e) {
-        //              e.printStackTrace();
-        //            } catch (Exception e) {
-        //              e.printStackTrace();
-        //            }
-        //          }
-        //        }
       }
 
     }
@@ -2911,24 +2694,12 @@ class DecodeHandler extends Handler {
       ocrResult = new OcrResult();
       ocrResult.setWordConfidences(baseApi.wordConfidences());
       ocrResult.setMeanConfidence( baseApi.meanConfidence());
-      if (ViewfinderView.DRAW_REGION_BOXES) {
-        ocrResult.setRegionBoundingBoxes(baseApi.getRegions().getBoxRects());
-      }
-      if (ViewfinderView.DRAW_TEXTLINE_BOXES) {
-        ocrResult.setTextlineBoundingBoxes(baseApi.getTextlines().getBoxRects());
-      }
-      if (ViewfinderView.DRAW_STRIP_BOXES) {
-        ocrResult.setStripBoundingBoxes(baseApi.getStrips().getBoxRects());
-      }
 
       // Always get the word bounding boxes--we want it for annotating the bitmap after the user
       // presses the shutter button, in addition to maybe wanting to draw boxes/words during the
       // continuous mode recognition.
       ocrResult.setWordBoundingBoxes(baseApi.getWords().getBoxRects());
 
-//      if (ViewfinderView.DRAW_CHARACTER_BOXES || ViewfinderView.DRAW_CHARACTER_TEXT) {
-//        ocrResult.setCharacterBoundingBoxes(baseApi.getCharacters().getBoxRects());
-//      }
     } catch (RuntimeException e) {
       Log.e("OcrRecognizeAsyncTask", "Caught RuntimeException in request to Tesseract. Setting state to CONTINUOUS_STOPPED.");
       e.printStackTrace();
