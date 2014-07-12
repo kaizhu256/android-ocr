9dda91c2613a93c329753dd1f817c4782331f96e
kai zhu <kaizhu256@gmail.com>
2014-07-12 17:31:08 +0800

    sync

--- android/src/edu/sfsu/cs/orange/ocr/CaptureActivity.java
+++ android/src/edu/sfsu/cs/orange/ocr/CaptureActivity.java
@@ -561,12 +561,6 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
       startActivity(intent);
       break;
     }
-    case ABOUT_ID: {
-      intent = new Intent(this, HelpActivity.class);
-      intent.putExtra(HelpActivity.REQUESTED_PAGE_KEY, HelpActivity.ABOUT_PAGE);
-      startActivity(intent);
-      break;
-    }
     }
     return super.onOptionsItemSelected(item);
   }
@@ -1040,19 +1034,6 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
         isFirstLaunch = true;
       } else {
         isFirstLaunch = false;
-      }
-      if (currentVersion > lastVersion) {
-        
-        // Record the last version for which we last displayed the What's New (Help) page
-        prefs.edit().putInt(PreferencesActivity.KEY_HELP_VERSION_SHOWN, currentVersion).commit();
-        Intent intent = new Intent(this, HelpActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-        
-        // Show the default page on a clean install, and the what's new page on an upgrade.
-        String page = lastVersion == 0 ? HelpActivity.DEFAULT_PAGE : HelpActivity.WHATS_NEW_PAGE;
-        intent.putExtra(HelpActivity.REQUESTED_PAGE_KEY, page);
-        startActivity(intent);
-        return true;
       }
     } catch (PackageManager.NameNotFoundException e) {
       Log.w(TAG, e);
